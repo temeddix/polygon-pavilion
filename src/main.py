49bad1429526d1f89b4d5f88d8132913491de0f6
Danny Kim <temeddix@gmail.com>
2025-10-30 00:45:12 +0900

    Make closed polyline curve from pieces

--- src/main.py
+++ src/main.py
@@ -2,10 +2,10 @@ from collections.abc import Sequence
 from importlib import import_module
 from typing import NamedTuple, Optional, Union
 
-from Rhino.Geometry import Brep, Curve, GeometryBase, Point3d
+from Rhino.Geometry import Brep, Curve, GeometryBase, Point3d, PolylineCurve
 from Rhino.Geometry.Intersect import Intersection
 
-TOLERANCE = 0.01
+TOLERANCE = 0.001
 
 
 class GeometryInput(NamedTuple):
@@ -39,11 +39,17 @@ def join_curves(curves: Sequence[Curve]) -> Curve:
     return joined[0]
 
 
-def points_to_closed_curve(points: Sequence[Point3d]) -> Curve:
-    module = import_module("ghpythonlib.components")
-    func = getattr(module, "PointsToClosedCurve")
-    result = func(points)
-    return result
+def points_to_closed_polyline_curve(points: Sequence[Point3d]) -> PolylineCurve:
+    return PolylineCurve([*points, points[0]])
+
+
+def extract_vertices(curve: Curve) -> list[Point3d]:
+    segments = curve.DuplicateSegments()
+    points: list[Point3d] = []
+    if not curve.IsClosed:
+        points.append(next(iter(segments)).PointAtStart)
+    points.extend(segment.PointAtEnd for segment in segments)
+    return points
 
 
 def main():
@@ -61,12 +67,16 @@ def main():
         join_curves(list(Intersection.BrepBrep(cell, shape, TOLERANCE)[1]))
         for cell in voronoi_cells
     ]
+    refined_pieces = [
+        points_to_closed_polyline_curve(extract_vertices(piece)) for piece in raw_pieces
+    ]
 
     return GeometryOutput(
-        result=raw_pieces,
+        result=refined_pieces,
         debug_shapes=[
             populated_points,
             voronoi_cells,
+            raw_pieces,
         ],
     )
 
